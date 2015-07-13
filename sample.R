782f272154effb0c22be88cf34328477d84d9851
Watal M. Iwasaki <heavy.watal@gmail.com>
2015-07-13 23:53:38 +0900

    Calc summary statistics in sample.R

--- sample.R
+++ sample.R
@@ -14,7 +14,57 @@ history = read_tsv(file.path(indir, 'mutation_history.tsv.gz')) %>>% (?.)
 
 population = read_tsv(file.path(indir, 'population.tsv.gz'), col_types='iicd') %>>% (?.)
 
-population %>>%
+.sample = population %>>%
     mutate(half=ifelse(x<1, 'left', 'right')) %>>%
     group_by(half) %>>%
-    sample_n(5)
+    sample_n(5) %>>%
+    mutate(serial=seq_len(n())) %>>% (?.)
+
+
+# summary statistics
+## the number of distinct CNAs (different strings)
+
+.unnested = .sample %>>%
+    mutate(site=strsplit(sites, '\\|'), sites=NULL) %>>%
+    unnest(site) %>>%
+    mutate(site=as.integer(site)) %>>%
+    (?.)
+
+.sites = unique(.unnested$site)
+length(.sites)
+
+## Shannon index of binary patterns
+
+.binary = .unnested %>>%
+    group_by(half, serial) %>>%
+    do(data.frame(site=.sites, exists=.sites %in% .$site)) %>>%
+    ungroup %>>%
+    arrange(site) %>>%
+    (?.)
+
+.binary %>>%
+    group_by(site) %>>%
+    summarise(p= sum(exists) / n()) %>>%
+    summarise(entropy=sum(-p * log(p) - (1-p) * log(1 - p)))
+
+## the total number of alterations
+
+nrow(.unnested)
+
+## the number of variegated alterations
+## the number of side-variegated alterations
+
+.binary %>>% as.data.frame
+
+.binary %>>%
+    group_by(site, half) %>>%
+    summarise(any=any(exists), all=all(exists))
+
+#  lhs rhs
+#  --- ---: ERROR
+#  --- any: regional
+#  --- all: side-specific
+#  any any: variegated
+#  any all: side-variegated
+#  all all: public
+
