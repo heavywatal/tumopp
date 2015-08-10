a0b241e413bd1ed368c3b90b6b5720c3945f6e0e
Watal M. Iwasaki <heavy.watal@gmail.com>
2015-08-10 17:29:46 +0900

    Add function to draw serial sections. #1

--- plot.R
+++ plot.R
@@ -5,6 +5,7 @@ library(tidyr)
 library(plyr)
 library(dplyr)
 library(ggplot2)
+library(animation)
 
 .argv = commandArgs(trailingOnly=TRUE)
 indir = .argv[1]
@@ -12,9 +13,10 @@ indir = ifelse(is.na(indir), '.', indir)
 
 conf = wtl::read.conf(file.path(indir, 'program_options.conf')) %>>% (?.)
 history = read_tsv(file.path(indir, 'mutation_history.tsv.gz')) %>>% (?.)
-population = read_tsv(file.path(indir, 'population.tsv.gz'), col_types='iicd') %>>% (?.)
+#population = read_tsv(file.path(indir, 'population.tsv.gz'), col_types='iicd') %>>% (?.)
+population = read_tsv(file.path(indir, 'population.tsv.gz')) %>>% (?.)
 
-.data = population %>>%
+unnested = population %>>%
     mutate(sites=strsplit(sites, '\\|')) %>>%
     unnest(sites) %>>%
     bind_rows(population %>>% filter(sites=='')) %>>%
@@ -52,15 +54,52 @@ plot_mutation_history_2d = function(.data) {.data %>>%
 
 #########1#########2#########3#########4#########5#########6#########7#########
 
+plot_early_mutations_3d = function(.z=20, .data) {.data %>>%
+    filter(z==.z) %>>%
+    bind_rows(expand.grid(x=maxabs+2, y=maxabs+2, z=.z,
+                          fitness=-1, size=as.factor(seq_len(8)))) %>>%
+    ggplot(aes(x, y, fill=size))+
+    geom_raster(alpha=0.66)+
+    geom_hline(yintercept=.z)+
+    scale_fill_hue(na.value='white')+
+    coord_cartesian(x=c(-maxabs, maxabs), y=c(-maxabs, maxabs))+
+    theme(panel.background=element_rect(fill='grey80'))+
+    theme(panel.grid=element_blank())+
+    theme(axis.title=element_blank())+
+    theme(legend.position='right')
+}
+#plot_early_mutations_3d(15)
+
+#########1#########2#########3#########4#########5#########6#########7#########
+
 if (conf[['dimensions']] == 2) {
 
 .threshold = history$size %>>% nth(4)
-.p = plot_early_mutations_2d(.data, .threshold)
+.p = plot_early_mutations_2d(unnested, .threshold)
 #.p
 ggsave('early_mutations.png', .p, width=7, height=7)
 
-.p = plot_mutation_history_2d(.data)
+.p = plot_mutation_history_2d(unnested)
 #.p
 ggsave('gradient.png', .p, width=7, height=7)
 
+} else {
+
+early3d = unnested %>>%
+    mutate(size=ifelse(size <= 8, size, NA), effect=NULL) %>>%
+    dplyr::select(-starts_with('origin_')) %>>%
+    filter(!duplicated(.)) %>>%
+    group_by(x, y, z) %>>%
+    filter(!(n() > 1 & is.na(size))) %>>% ungroup %>>%
+    mutate(size=as.factor(size)) %>>% (?.)
+
+maxabs = with(population, max(abs(c(x, y, z)))) %>>% (?.)
+
+animation::saveGIF({
+    for (i in seq(-maxabs, maxabs)) {
+        print(plot_early_mutations_3d(i, early3d))
+    }},
+    'animation.gif', loop=TRUE, interval=0.15, outdir=getwd())
+
 }
+
