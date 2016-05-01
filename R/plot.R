6630df67635301fd96f3a2a4a9d05512acf250a1
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-05-01 11:32:28 +0900

    Add function for serial section plot

--- R/plot.R
+++ R/plot.R
@@ -14,3 +14,22 @@ gglattice2D = function(.data, colour='ancestors', limit=maxabs(.data)) {
     ggplot2::theme(panel.grid=element_blank())+
     ggplot2::theme(axis.title=element_blank())
 }
+
+#' Write animation GIF for serial sections of 3D tumor
+#' @param filename a string
+#' @param .data a data.frame
+#' @param width an integer
+#' @param height an integer
+#' @rdname plot
+#' @export
+save_serial_section = function(filename='serial_section.gif', .data, width=720, height=640) {
+    .lim = maxabs(.data)
+    section_plots = dplyr::group_by(.data, z) %>%
+        dplyr::do(plt={
+            gglattice2D(., limit=.lim)+
+            ggplot2::geom_hline(yintercept=.$z[1])
+        })
+    animation::saveGIF({for (p in section_plots$plt) {print(p)}},
+        filename, outdir=getwd(), interval=0.15,
+        ani.width=width, ani.height=height, loop=TRUE, autobrowse=FALSE)
+}
