38dd57dd95ac025489e96060828c0517f2899468
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-05-15 17:10:16 +0900

    Split source files

--- R/coord.R
+++ R/coord.R
@@ -1,25 +1,3 @@
-#' calculate radius from volume
-#' @param volume a numeric vector
-#' @return radius a numeric vector
-#' @rdname sphere
-#' @export
-#' @examples
-#' sphere_radius(42)
-sphere_radius = function(volume) {
-    (volume * 3 / (4 * pi)) ^ (1/3)
-}
-
-#' calculate volume from radius
-#' @param radius a numeric vector
-#' @return volume a numeric vector
-#' @rdname sphere
-#' @export
-#' @examples
-#' sphere_volume(42)
-sphere_volume = function(radius) {
-    4 * pi * (radius^3) / 3
-}
-
 #' get max value for plot limits
 #' @param mtrx a data.frame with (x, y, z) columns
 #' @return max(abs(x, y, z))
--- R/read.R
+++ R/read.R
@@ -53,93 +53,9 @@ read_snapshots = function(conf) {
     })
 }
 
-#' extract param names from conf
-#' @param conf a data.frame
-#' @return a string vector
-#' @rdname extract
-#' @export
-altered_params = function(conf) {
-    dplyr::select_(~conf, ~-path, ~-out_dir, ~-seed) %>>%
-    dplyr::summarise_each(dplyr::funs(length(unique(.)))) %>>%
-    unlist() %>>% (.[. > 1]) %>>% names()
-}
-
-#' extract demography from population data
-#' @param grouped_df a grouped_df
-#' @return a grouped data.frame
-#' @rdname extract
-#' @export
-extract_demography = function(grouped_df) {grouped_df %>>%
-    dplyr::select_(~birth, ~death) %>>%
-    tidyr::gather_(~event, ~time, ~birth, ~death) %>>%
-    dplyr::filter_(~!(time == 0 & event == 'death')) %>>%  # alive
-    dplyr::mutate_(event=~ factor(event, levels=c('death', 'birth'))) %>>%
-    dplyr::arrange_(~time, ~event) %>>%
-    dplyr::mutate_(dn =~ ifelse(event == 'birth', 1, -1),
-           size =~ cumsum(dn)) %>>%
-    dplyr::group_by_(~time, add=TRUE) %>>%
-    dplyr::summarise_(size=~ dplyr::last(size))
-}
-
-#' cell ids that existed at the specified tumor size
-#' @param population a raw data.frame
-#' @param size an integer
-#' @param origin an integer
-#' @return an integer vector
-#' @rdname extract
-#' @export
-exclusive_ancestors = function(population, size, origin=1) {
-    mothers = population %>>%
-        head(size - origin) %>>%
-        `[[`('genealogy') %>>%
-        strsplit(':') %>>%
-        sapply(dplyr::last)
-    seq(length(mothers) + size) %>>% setdiff(mothers)
-}
-
-#' cell ids that existed at the specified tumor size (from snapshots)
-#' @param snapshots a data.frame
-#' @return an integer vector
-#' @rdname extract
-#' @export
-exclusive_ancestors_ss = function(snapshots, size) {
-    dplyr::group_by_(~snapshots, ~time) %>>%
-    dplyr::filter_(~ n() == size) %>>%
-    (strsplit(.$genealogy, ':')) %>>%
-    sapply(dplyr::last)
-}
-
-#' split ancestor string and extract one matched
-#' @param genealogy a string vector of colon-separated ancestors
-#' @param ids a string vector of exclusive ancestor ids
-#' @return a string vector of extracted ancestors
-#' @rdname extract
-#' @export
-#' @examples
-#' extract_ancestor(c('1:2:9', '1:2:4:6'), c('2', '5', '6'))
-extract_ancestor = function(genealogy, ids) {
-    sapply(stringr::str_split(genealogy, ':', length(ids) + 1), function(x) {
-        dplyr::last(x[x %in% ids])
-    })
-}
-
-#' E ancestor column
-#' @param raw_population a data.frame including ancestors
-#' @param n an integer number of exclusive ancestors
-#' @return a data.frame
-#' @rdname extract
-#' @export
-colorcode_survivors = function(raw_population, n=4) {
-    num_founders = length(grep(':', raw_population$genealogy, invert=TRUE))
-    anc_ids = exclusive_ancestors(raw_population, n, num_founders)
-    raw_population %>>%
-        dplyr::filter_(~ death == 0) %>>%
-        dplyr::mutate_(ancestor=~ extract_ancestor(genealogy, anc_ids))
-}
-
 #' get commandline arguments
 #' @return a list
-#' @rdname extract
+#' @rdname read
 #' @export
 command_args = function() {
     .argv = commandArgs(trailingOnly=FALSE)
