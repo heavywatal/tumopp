3e12f16133efbdafddee9508cc0dda614a9977e4
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-06-06 18:28:18 +0900

    Clean post.R

--- post.R
+++ post.R
@@ -1,7 +1,5 @@
 #!/usr/bin/env Rscript
 library(pipeR)
-library(readr)
-library(tidyr)
 library(dplyr)
 library(ggplot2)
 
@@ -16,20 +14,7 @@ if (!is.na(indir)) {
 }
 
 conf = read_conf()
-population = read_population(conf) %>>%
-    colorcode_survivors()
-
-#########1#########2#########3#########4#########5#########6#########7#########
-if (conf[['dimensions']] == 2) {
-
-.p = population %>>%
-    gglattice2d('ancestor')
-.p
-ggsave('colorcode2d.png', .p, width=8, height=7)
-
-#########1#########2#########3#########4#########5#########6#########7#########
-} else {  # 3D
-#########1#########2#########3#########4#########5#########6#########7#########
+population = read_population(conf) %>>% colorcode_survivors()
 
 population %>>%
     dplyr::filter(z == 0) %>>%
@@ -42,6 +27,9 @@ population %>>%
     gglattice2d('ancestor') %>>%
     (ggsave('section_z0_surface.png', ., width=8, height=7))
 
+#########1#########2#########3#########4#########5#########6#########7#########
+if (conf[['dimensions']] > 2) {
+
 library(rgl)
 if (rgl.cur()) {rgl.close()}
 rgl::open3d(windowRect=c(0, 0, 600, 600))
@@ -53,5 +41,5 @@ writeWebGL('.', 'rgl.html', snapshot=FALSE, width=600, height=600)
 rgl::snapshot3d('colorcode3d.png')
 
 #########1#########2#########3#########4#########5#########6#########7#########
-}  # fi 2D/3D
+}  # fi 3D
 #########1#########2#########3#########4#########5#########6#########7#########
