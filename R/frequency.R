c8e46d6a2497ddc4dc6b7b620a0dc42943c729f3
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-07-25 19:32:01 +0900

    Use stats::mad() for math_score()

--- R/frequency.R
+++ R/frequency.R
@@ -1,10 +1,10 @@
-#' Calculate MATH score from variant allele freqneucies
-#' @param freqs numeric vector
+#' Calculate MATH (mutant-allele tumor heterogeneity) score from variant allele freqneucies
+#' @inheritParams stats::mad
 #' @return numeric
 #' @rdname frequency
 #' @export
-math_score = function(freqs) {
-    med = stats::median(freqs)
-    mad = stats::median(abs(freqs - med))
+math_score = function(x, constant=1.4826, na.rm=FALSE) {
+    med = stats::median(x)
+    mad = stats::mad(x, center=med, constant=constant, na.rm=FALSE)
     mad / med
 }
