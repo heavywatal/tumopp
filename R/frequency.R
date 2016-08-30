86b2ddfc326b3f81f41c78a9afaf5fb19c02e0b8
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-08-30 14:22:36 +0900

    Update frequency functions

--- R/frequency.R
+++ R/frequency.R
@@ -4,18 +4,18 @@
 #' @rdname frequency
 #' @export
 math_score = function(x, constant=1.4826, na.rm=FALSE) {
-    med = stats::median(x)
-    mad = stats::mad(x, center=med, constant=constant, na.rm=FALSE)
+    med = stats::median(x, na.rm=na.rm)
+    mad = stats::mad(x, center=med, constant=constant, na.rm=na.rm)
     mad / med
 }
 
-#' Make true spectrum from which allele frequencies are generated
+#' Translate descendant numbers to frequencies
 #' @param descendants column of raw population including ancestors
 #' @param lowerbound to discard peripheral branches
-#' @return numeric vector
+#' @return numeric vector applicable to math_score
 #' @rdname frequency
 #' @export
-descendants_fs = function(descendants, lowerbound=0) {
-    fs = descendants / max(descendants)
-    fs[(lowerbound < fs) & (fs < 1)]
+descfreqs = function(descendants, lowerbound=0) {
+    frac = descendants / max(descendants)
+    dplyr::if_else((lowerbound < frac) & (frac < 1), frac, as.double(NA))
 }
