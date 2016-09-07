69b39873ed8544fa717f9c193fe020369c326d42
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-09-07 19:09:01 +0900

    Flip coordinates of genealogy

--- R/graph.R
+++ R/graph.R
@@ -20,57 +20,58 @@ make_edgelist = function(population) {
 #' @export
 layout_genealogy = function(edgelist) {
     g = igraph::graph_from_data_frame(edgelist)
-    lo = igraph::layout.reingold.tilford(g, circular=FALSE)
+    lo = igraph::layout_as_tree(g, flip.y=FALSE)
     nodes = tibble::as_tibble(lo) %>>%
-        stats::setNames(c('x', 'y')) %>>%
+        stats::setNames(c('pos', 'age')) %>>%
         dplyr::mutate_(
           id= ~igraph::V(g)$name %>>% as.integer(),
-          extant= ~(igraph::degree(g, mode='out') == 0L),
-          y= ~max(y) - y)
+          extant= ~(igraph::degree(g, mode='out') == 0L))
     edgelist %>>%
     dplyr::left_join(dplyr::select_(nodes, ~-extant), by=c(from='id')) %>>%
-    dplyr::left_join(dplyr::select_(nodes, xend= ~x, yend= ~y, ~extant, to= ~id), by='to')
+    dplyr::left_join(dplyr::select_(nodes, posend= ~pos, ageend= ~age, ~extant, to= ~id), by='to')
 }
 
 #' Plot genealogy
 #' @param .data tibble from layout_genealogy()
-#' @param age_lim numeric vector
+#' @param label character or expression
 #' @param hist logical: whether add age_histogram() or not.
 #' @return gg
 #' @rdname graph
 #' @export
-ggplot_genealogy = function(.data, age_lim=c(0, max(.data$yend)), hist=TRUE) {
+ggplot_genealogy = function(.data, label='', hist=TRUE) {
+    age_lim=c(0, max(.data$ageend, 20))
     .tree = ggplot2::ggplot(.data)+
-    ggplot2::geom_segment(ggplot2::aes_(~x, ~y, xend=~xend, yend=~yend), alpha=0.3, size=0.3)+
+    ggplot2::geom_segment(ggplot2::aes_(~age, ~pos, xend=~ageend, yend=~posend), alpha=0.3, size=0.3)+
     ggplot2::geom_point(data=dplyr::filter_(.data, ~extant),
-        ggplot2::aes_(x=~xend, y=~yend),
-        size=1, colour='dodgerblue', alpha=0.2)+
-    ggplot2::scale_y_continuous(trans='reverse')+
-    ggplot2::coord_cartesian(ylim=age_lim)+
+        ggplot2::aes_(x=~ageend, y=~posend),
+        size=0.8, colour='dodgerblue', alpha=0.2)+
+    ggplot2::coord_cartesian(xlim=age_lim)+
     wtl::theme_wtl()+
     ggplot2::theme(
         axis.title=ggplot2::element_blank(),
         axis.text=ggplot2::element_blank(),
         axis.ticks=ggplot2::element_blank(),
-        panel.grid.major.x=ggplot2::element_blank())
+        panel.grid.major.y=ggplot2::element_blank())
     if (hist) {
         .hist = age_histogram(.data, age_lim)
-        gridExtra::arrangeGrob(.tree, .hist, nrow=1, widths=c(3, 1))
+        .top = grid::textGrob(label, x=grid::unit(0.1, 'npc'), just=c('left', 'top'))
+        gridExtra::arrangeGrob(.tree, .hist, nrow=2, heights=c(3, 1), top=.top)
     } else {.tree}
 }
 
 #' Plot age histogram
+#' @param age_lim numeric vector
 #' @return gg
 #' @rdname graph
 age_histogram = function(.data, age_lim) {
     dplyr::filter_(.data, ~extant) %>>%
-    ggplot2::ggplot(ggplot2::aes_(~yend))+
+    ggplot2::ggplot(ggplot2::aes_(~ageend))+
     ggplot2::geom_histogram(binwidth=1, center=0)+
-    ggplot2::scale_x_continuous(trans='reverse')+
-    ggplot2::coord_flip(xlim=age_lim)+
-    ggplot2::labs(x='Number of cell divisions')+
+    ggplot2::coord_cartesian(xlim=age_lim)+
     wtl::theme_wtl()+
     ggplot2::theme(
-        axis.title.x=ggplot2::element_blank(), axis.text.x=ggplot2::element_blank(),
-        axis.ticks.x=ggplot2::element_blank())
+        axis.title=ggplot2::element_blank(),
+        axis.text.y=ggplot2::element_blank(),
+        axis.ticks.y=ggplot2::element_blank(),
+        panel.grid.major.y=ggplot2::element_blank())
 }
