01282c01f1b08570573864fc5ff1956f96bc0a1d
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-11-02 20:35:03 +0900

    Add replicating option to within_between_samples()

--- R/summarize.R
+++ R/summarize.R
@@ -13,22 +13,27 @@ genetic_stats = function(extant) {
 
 #' Sample two specimens and measure mean branch lengths
 #' @param population tibble
-#' @param size number of cells
+#' @param ncell number of cells per specimen
+#' @param npair number of pairs and output rows
 #' @return tibble
 #' @rdname summarize
 #' @export
-within_between_samples = function(population, size=100L) {
-    .extant = filter_extant(population)
-    .o1 = dplyr::sample_n(.extant, 1)
-    .o2 = dplyr::sample_n(.extant, 1)
-    .nodes1 = sample_bulk(.extant, .o1, size)$id
-    .nodes2 = sample_bulk(.extant, .o2, size)$id
-    .within1 = mean_branch_length(population, .nodes1)
-    .within2 = mean_branch_length(population, .nodes2)
-    tibble::tibble(
-        euclidean= dist_euclidean(.o1, .o2),
-        within= mean(c(.within1, .within2)),
-        between= mean_branch_length(population, .nodes1, .nodes2))
+within_between_samples = function(population, ncell=100L, npair=1L) {
+    if (npair > 1L) {
+        purrr::map_df(seq_len(npair), ~within_between_samples(population, ncell))
+    } else {
+        .extant = filter_extant(population)
+        .o1 = dplyr::sample_n(.extant, 1)
+        .o2 = dplyr::sample_n(.extant, 1)
+        .nodes1 = sample_bulk(.extant, .o1, ncell)$id
+        .nodes2 = sample_bulk(.extant, .o2, ncell)$id
+        .within1 = mean_branch_length(population, .nodes1)
+        .within2 = mean_branch_length(population, .nodes2)
+        tibble::tibble(
+            euclidean= dist_euclidean(.o1, .o2),
+            within= mean(c(.within1, .within2)),
+            between= mean_branch_length(population, .nodes1, .nodes2))
+    }
 }
 
 #' Summary statistics of morphology
