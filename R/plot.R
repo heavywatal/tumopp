84e84f3eec93c658ffaaf1c59d4d56691a9695c4
Watal M. Iwasaki <heavy.watal@gmail.com>
2016-12-09 17:14:17 +0900

    Move plot functions

--- R/plot.R
+++ R/plot.R
@@ -12,7 +12,7 @@ ggfreqspec = function(freqs) {
 }
 
 #' ggplot for 2D lattice
-#' @param .data data.frame
+#' @param .data tbl with extant cells
 #' @param colour column name to colorcode
 #' @param size relative size of points
 #' @param alpha opacity [0, 1]
@@ -27,12 +27,57 @@ gglattice2d = function(.data, colour='clade', alpha=0.66, size=1, limit=max_abs_
     ggplot2::theme(axis.title=ggplot2::element_blank())
 }
 
+#########1#########2#########3#########4#########5#########6#########7#########
+
+#' Plot genealogy
+#' @param .data tbl from layout_genealogy()
+#' @param xmax numeric
+#' @param colour character
+#' @return gg
+#' @rdname plot-igraph
+#' @export
+plot_genealogy = function(.data, xmax=max(.data$ageend), colour='dodgerblue') {
+    ggplot2::ggplot(.data)+
+    ggplot2::geom_segment(ggplot2::aes_(~age, ~pos, xend=~ageend, yend=~posend), alpha=0.3, size=0.3)+
+    ggplot2::geom_point(data=dplyr::filter_(.data, ~extant),
+        ggplot2::aes_(x=~ageend, y=~posend), size=0.8, colour=colour, alpha=0.2)+
+    ggplot2::coord_cartesian(xlim=c(0, xmax), expand=FALSE)+
+    wtl::theme_wtl()+
+    ggplot2::theme(
+        axis.title.y=ggplot2::element_blank(),
+        axis.text.y=ggplot2::element_blank(),
+        axis.ticks.y=ggplot2::element_blank(),
+        panel.grid.major.y=ggplot2::element_blank())
+}
+
+#' Plot age histogram
+#' @param alpha opacity [0, 1]
+#' @param ... passed to aes_()
+#' @return gg
+#' @rdname plot-igraph
+#' @export
+plot_age_histogram = function(.data, xmax=max(.data$ageend), alpha=1.0, ...) {
+    dplyr::filter_(.data, ~extant) %>>%
+    ggplot2::ggplot(ggplot2::aes_(~ageend, ...))+
+    ggplot2::geom_histogram(position='identity', binwidth=1, center=0, alpha=alpha)+
+    ggplot2::coord_cartesian(xlim=c(0, xmax))+
+    wtl::theme_wtl()+
+    ggplot2::theme(
+        axis.title=ggplot2::element_blank(),
+        axis.text.y=ggplot2::element_blank(),
+        axis.ticks.y=ggplot2::element_blank(),
+        panel.grid.major.y=ggplot2::element_blank())
+}
+
+#########1#########2#########3#########4#########5#########6#########7#########
+
 #' Write animation GIF for serial sections of 3D tumor
+#' @param .data tbl with extant cells
 #' @param filename string
 #' @param ... passed to gglattice2d
 #' @param width integer
 #' @param height integer
-#' @rdname plot
+#' @rdname plot-animation
 #' @export
 save_serial_section = function(.data, filename='serial_section.gif', ..., width=720, height=640) {
     if (!requireNamespace('animation', quietly=TRUE)) {
