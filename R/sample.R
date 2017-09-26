8afc50da678f171831b0ca2118fca7b73b5e8bb8
Watal M. Iwasaki <heavy.watal@gmail.com>
2017-09-26 14:44:18 +0900

    Simplify function: make_samples() => sprinkle_mutations()

--- R/sample.R
+++ R/sample.R
@@ -17,34 +17,29 @@ sample_bulk = function(population, center, size=100L) {
     utils::head(size)
 }
 
-#' Make ms-like matrix from genealogy column of population
+#' Make ms-like neutral variation matrix from genealogy column of extant cells
 #' @param genealogy list of int vectors
-#' @param nsam number of samples
 #' @param mu mutation rate per cell division (ignored if segsites is given)
 #' @param segsites number of segregating sites
 #' @return int matrix [nsam x segsites]
 #' @rdname sample
 #' @export
-make_samples = function(genealogy, nsam=length(genealogy), mu=NULL, segsites=NULL) {
-    if (nsam < length(genealogy)) {
-        indices = sample.int(length(genealogy), nsam, replace=FALSE)
-        genealogy = genealogy[sort(indices)]
-    }
+sprinkle_mutations = function(genealogy, mu=NULL, segsites=NULL) {
     common_ancs = purrr::reduce(genealogy, intersect)
     nodes = purrr::flatten_int(genealogy) %>% unique() %>% setdiff(common_ancs)
     if (is.null(segsites)) {
         if (is.null(mu)) stop('specify either mu or segsites')
         segsites = stats::rpois(1L, length(nodes) * mu)
     } else if (!is.null(mu)) warning('mu is ignored if segsites is given')
-    mutant_ids = sample(nodes, segsites, replace=TRUE)
+    mutant_ids = sample(nodes, segsites, replace=TRUE) %>% sort()
     genealogy %>%
         purrr::map(~ mutant_ids %in% .) %>%
         purrr::flatten_int() %>%
-        matrix(nrow=nsam, byrow=TRUE)
+        matrix(nrow=length(genealogy), byrow=TRUE)
 }
 
 #' Summary statistics of neutral mutations
-#' @param msout tibble from `make_samples()`
+#' @param msout tibble from `sprinkle_mutations()`
 #' @param lowerbound to discard peripheral branches
 #' @return tibble
 #' @rdname summarize
