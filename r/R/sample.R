e8018e48c0a38690433101097b1dcd25691e03b6
Watal M. Iwasaki <heavy.watal@gmail.com>
2018-03-05 15:23:54 +0900

    :boom: Separate sample_random_regions() from within_between_samples()
    Put both functions in sample.R

--- r/R/sample.R
+++ r/R/sample.R
@@ -13,6 +13,49 @@ sample_bulk = function(extant, center, size=100L) {
     utils::head(size)
 }
 
+#' Sample multiple regions from extant cells
+#' @param ncell number of cells per specimen
+#' @return nested tibble
+#' @rdname sample
+#' @export
+sample_random_regions = function(extant, nsam=2L, ncell=10L) {
+  stopifnot(nsam > 1L)
+  extant %>%
+    dplyr::select(.data$id, .data$x, .data$y, .data$z) %>%
+    dplyr::sample_n(nsam) %>%
+    dplyr::mutate(samples = purrr::pmap(., function(x, y, z, ...) {
+      sample_bulk(extant, center = c(x = x, y = y, z = z), size = ncell)$id
+    }))
+}
+
+#' Sample specimens and measure mean branch lengths
+#' @param regions output of sample_random_regions()
+#' @param graph igraph
+#' @return tibble
+#' @rdname sample
+#' @export
+within_between_samples = function(regions, graph) {
+  regions %>%
+    dplyr::mutate(
+      within = purrr::map_dbl(.data$samples, ~mean_branch_length.igraph(graph, as.character(.x)))
+    ) %>%
+    purrr::transpose() %>%
+    purrr::cross2(.x=., .y=., .filter=~.x$id >= .y$id) %>%
+    purrr::map_dfr(~{
+      row_i = .x[[1L]]
+      row_j = .x[[2L]]
+      tibble::tibble(
+        region_i = row_i$id,
+        region_j = row_j$id,
+        within_i = row_i$within,
+        within_j = row_j$within,
+        euclidean = dist_euclidean(row_i, row_j),
+        between = mean_branch_length.igraph(.graph, as.character(row_i$samples[[1L]]), as.character(row_j$samples[[1L]]))
+      )
+    }) %>%
+    dplyr::mutate(within = 0.5 * (.data$within_i + .data$within_j))
+}
+
 #' Traceback ancestors and calculate union of the IDs
 #' @param population tibble of raw population
 #' @param samples vector of IDs
--- r/R/summarize.R
+++ r/R/summarize.R
@@ -12,41 +12,6 @@ genetic_stats = function(extant) {
   )
 }
 
-#' Sample specimens and measure mean branch lengths
-#' @param population tibble
-#' @param ncell number of cells per specimen
-#' @param nsam number of samples
-#' @return tibble
-#' @rdname summarize
-#' @export
-within_between_samples = function(population, ncell=100L, nsam=2L) {
-  stopifnot(nsam > 1L)
-  .extant = filter_extant(population)
-  .centers = .extant %>%
-    dplyr::select(.data$x, .data$y, .data$z) %>%
-    dplyr::sample_n(nsam) %>%
-    dplyr::mutate(
-      sample_id = seq_along(.data$x),
-      samples = purrr::pmap(., function(x, y, z, ...) {
-        sample_bulk(.extant, center = c(x = x, y = y, z = z), size = ncell)$id
-      }),
-      within = purrr::map_dbl(.data$samples, ~mean_branch_length(population, .x))
-    )
-  tidyr::crossing(lhs = .centers$sample_id, rhs = .centers$sample_id) %>%
-    dplyr::filter(.data$lhs < .data$rhs) %>%
-    dplyr::mutate(res = purrr::pmap(., ~{
-      row_x = .centers[.x, ]
-      row_y = .centers[.y, ]
-      tibble::tibble(
-        euclidean = dist_euclidean(row_x, row_y),
-        within = mean(row_x$within, row_y$within),
-        between = mean_branch_length(population, row_x$samples[[1]], row_y$samples[[1]])
-      )
-    })) %>%
-    tidyr::unnest() %>%
-    dplyr::select(-.data$lhs, -.data$rhs)
-}
-
 #' Summary statistics of morphology
 #' @return tibble
 #' @rdname summarize
