c0fe89a8dcf2da42dc9d409374e662142e4dac38
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-08-29 11:01:11 +0900

    :white_check_mark: Separate test/coord.cpp

---
+++ test/coord.cpp
@@ -0,0 +1,32 @@
+#include "coord.hpp"
+
+#include <wtl/iostr.hpp>
+
+template <class T> inline
+void test_coordinate(const std::valarray<int>& v) {
+    T coord(static_cast<unsigned>(v.size()));
+    std::cout << coord.core() << "\n";
+    std::cout << coord.sphere(20) << "\n";
+    for (auto x: coord.directions()) {
+        std::cout << x  << ": " << coord.euclidean_distance(x) << "\n";
+    }
+    std::cout << coord.neighbors(v) << "\n";
+    std::cout << coord.graph_distance(v) << "\n";
+    std::cout << coord.euclidean_distance(v) << "\n";
+    std::cout << coord.radius(100) << "\n";
+    std::cout << coord.radius(1000) << "\n";
+}
+
+int main() {
+    std::cout.precision(15);
+    const std::valarray<int> v2{3, -2};
+    test_coordinate<tumopp::Neumann>(v2);
+    test_coordinate<tumopp::Moore>(v2);
+    test_coordinate<tumopp::Hexagonal>(v2);
+
+    const std::valarray<int> v3{3, -2, 1};
+    test_coordinate<tumopp::Neumann>(v3);
+    test_coordinate<tumopp::Moore>(v3);
+    test_coordinate<tumopp::Hexagonal>(v3);
+    return 0;
+}
--- test/tissue.cpp
+++ test/tissue.cpp
@@ -1,22 +1,6 @@
 #include "tissue.hpp"
 
-#include <wtl/iostr.hpp>
-
-//! Test coordinate system
-template <class T> inline
-void test_coordinate(const std::valarray<int>& v) {
-    T coord(static_cast<unsigned>(v.size()));
-    std::cout << coord.core() << "\n";
-    std::cout << coord.sphere(20) << "\n";
-    for (auto x: coord.directions()) {
-        std::cout << x  << ": " << coord.euclidean_distance(x) << "\n";
-    }
-    std::cout << coord.neighbors(v) << "\n";
-    std::cout << coord.graph_distance(v) << "\n";
-    std::cout << coord.euclidean_distance(v) << "\n";
-    std::cout << coord.radius(100) << "\n";
-    std::cout << coord.radius(1000) << "\n";
-}
+#include <iostream>
 
 int main() {
     std::cout.precision(15);
@@ -25,15 +9,5 @@ int main() {
     tissue.grow(10);
     std::cout << tissue << "\n";
     std::cout << tissue.specimens();
-
-    const std::valarray<int> v2{3, -2};
-    test_coordinate<tumopp::Neumann>(v2);
-    test_coordinate<tumopp::Moore>(v2);
-    test_coordinate<tumopp::Hexagonal>(v2);
-
-    const std::valarray<int> v3{3, -2, 1};
-    test_coordinate<tumopp::Neumann>(v3);
-    test_coordinate<tumopp::Moore>(v3);
-    test_coordinate<tumopp::Hexagonal>(v3);
     return 0;
 }
