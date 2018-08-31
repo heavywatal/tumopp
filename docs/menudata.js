64ef2974321000a04b4d8c57d7d7d2aa0b2fcd20
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-09-01 00:52:35 +0900

    :memo: Doxygen [ci skip]

--- docs/menudata.js
+++ docs/menudata.js
@@ -50,7 +50,6 @@ var menudata={children:[
 {text:"w",url:"functions.html#index_w"},
 {text:"~",url:"functions.html#index_0x7e"}]},
 {text:"Functions",url:"functions_func.html",children:[
-{text:"b",url:"functions_func.html#index_b"},
 {text:"c",url:"functions_func.html#index_c"},
 {text:"d",url:"functions_func.html#index_d"},
 {text:"e",url:"functions_func.html#index_e"},
