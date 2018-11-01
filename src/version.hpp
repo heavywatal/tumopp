12b69954a3051fc4fec9de07f40504792a5d9472
Watal M. Iwasaki <heavywatal@users.noreply.github.com>
2018-11-01 14:12:27 +0900

    :art: Separate compilation unit of PROJECT_VERSION

---
+++ src/version.hpp
@@ -0,0 +1,12 @@
+#pragma once
+#ifndef TUMOPP_VERSION_HPP_
+#define TUMOPP_VERSION_HPP_
+
+namespace tumopp {
+
+extern const char* const PROJECT_NAME;
+extern const char* const PROJECT_VERSION;
+
+}
+
+#endif // TUMOPP_VERSION_HPP_
